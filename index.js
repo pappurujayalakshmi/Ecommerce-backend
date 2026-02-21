801f6e4a2a48e0c68df423518891de8f2964bc36
pappurujayalakshmi <pappurujayalakshmi4@gmail.com>
2026-02-21 12:11:16 +0530

    fixed

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-let mongoose=require("mongoose")
-let express=require("express")
-let cors=require("cors")
-require("dotenv").config()
-const rt = require("./routes/rt")
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    app.listen(process.env.port||5000)
-})
-let app=express()
-app.listen(5000)
-app.use(express.json())
-app.use(express.urlencoded({"extended":true}))
-app.use(cors())
-app.use("/pimgs",express.static("./prodimgs"))
-app.use("/",rt)
+let mongoose = require("mongoose");
+let express = require("express");
+let cors = require("cors");
+require("dotenv").config();
+
+const rt = require("./routes/rt");
+
+let app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/pimgs", express.static("./prodimgs"));
+app.use("/", rt);
+
+// Connect DB
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB Connected");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => console.log(err));
